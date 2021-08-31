89d567f28164e366a2556b05917a77937c4739dc
Jonhe <Jonhenrylangerman@gmail.com>
2021-08-30 21:52:51 -0700

    okay its working now

--- Develop/routes/apiRoutes/apiRoutes.js
+++ Develop/routes/apiRoutes/apiRoutes.js
@@ -1,41 +1,27 @@
-
-
-
-
-
 const fs = require('fs');
 const path = require('path');
 const router = require('express').Router();
-let database = require('../../db/db.json');
+let { database } = require('../../db/db.json');
 
+  router.get("/notes" , (req,res) => {
+    res.json(database);
+  });
 
-router.get('/notes', (req,res) => {
-  res.json(database);
-})
+  router.post("/notes", (req,res) => {
+    req.body.id = database.length.toString();
+    const note = createNewNote(req.body, database);
+    res.json(database);
+  });
 
-router.post('/notes', (req,res) => {
-  
-  console.log(req.body);
-  req.body.id = database.length.toString();
-  const note = createNewNote(req.body, database);
-  res.json(database);
-})
-
-router.get('/notes/:id', (req,res) => {
-  res.json(database);
-})
-
-router.put('/notes/:id', (req,res) => {
-  res.json(database);
-})
-
+  router.put("/notes/:id", (req,res) => {
+    res.json(database);
+  });
 
 
 function createNewNote(body, notes) {
   const note = body;
-  //console.log(note);
   notes.push(note);
-  console.log(notes);
+  
   fs.writeFileSync(
     path.join(__dirname, '../../db/db.json'),
     JSON.stringify({database:notes}, null, 2)
