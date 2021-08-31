61073f08786d7eaf2e84e33af40cccf5a66f4488
Jonhe <Jonhenrylangerman@gmail.com>
2021-08-30 20:21:03 -0700

    heroko add

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3002;
+const PORT = process.env.PORT || 3001;
 
 const apiRoutes = require('./routes/apiRoutes/apiRoutes.js');
 const htmlRoutes = require('./routes/htmlRoutes/htmlRoutes.js');
